66110b745a6f9cfc71755736fea80a069b85a79b
Mai Hoang <mnhoang.80@gmail.com>
2017-05-06 07:09:29 -0700

    Finish

--- config/orm.js
+++ config/orm.js
@@ -1,52 +1,80 @@
 var connection = require('../config/connection.js');
 
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        if (Object.hasOwnProperty.call(ob, key)) {
+            arr.push(key + "=" + ob[key]);
+        }
+    }
+
+    return arr.toString();
+}
+
 var orm = {
-  selectAll: function(tableName, cbFunction) {
-    var queryString = "SELECT * FROM " + tableName + ";";
-    connection.query(queryString, function(err, resultsArr) {
-      if (err) {
-        throw err;
-      }
-      cbFunction(resultsArr);
-    });
-  },
-  insertOne: function(tableName, cols, vals, cbFunction) {
-    var query = "INSERT INTO " + tableName + " (" + cols.toString(); + ") ";
-    	query += "VALUES (" + printQuestionMarks(vals.length); + ") ";
+    
+    selectAll: function(tableName, cbFunction) {
+        var queryString = "SELECT * FROM " + tableName + ";";
+        connection.query(queryString, function(err, resultsArr) {
+            if (err) {
+                throw err;
+            }
+            cbFunction(resultsArr);
+        });
+    },
 
-    console.log(query);
+    insertOne: function(tableName, obj, cbFunction) {
+        var query = "INSERT INTO " + tableName + " SET ?";
 
-    connection.query(query, vals, function(err, resultsArr) {
-      if (err) {
-        throw err;
-      }
-      cbFunction(resultsArr);
-    });
-  },
+        console.log(query);
 
-  updateOne: function(tableName, objColVals, condition, cbFunction) {
-    var query = "UPDATE " + tableName + " SET " + objToSql(objColVals);
-    	query += " WHERE " + condition;
+        connection.query(query, {
+                name: obj.name,
+                devoured: false,
+            },
 
-    console.log(query);
-    connection.query(query, function(err, resultsArr) {
-      if (err) {
-        throw err;
-      }
-      cbFunction(resultsArr);
-    });
-  }
-  // ,
-  // delete: function(tableName, condition, cbFunction) {
-  //   var query = "DELETE FROM " + tableName + " WHERE " + condition;
+            function(err, resultsArr) {
+                if (err) {
+                    throw err;
+                }
+                cbFunction(resultsArr);
+            });
+    },
 
-  //   connection.query(query, function(err, resultsArr) {
-  //     if (err) {
-  //       throw err;
-  //     }
-  //     cbFunction(resultsArr);
-  //   });
-  // }
+    updateOne: function(tableName, objColVals, condition, cbFunction) {
+        var query = "UPDATE " + tableName + " SET " + objToSql(objColVals);
+        query += " WHERE " + condition;
+
+        console.log(query);
+        connection.query(query, function(err, resultsArr) {
+            if (err) {
+                throw err;
+            }
+            cbFunction(resultsArr);
+        });
+    },
+
+    delete: function(tableName, condition, cbFunction) {
+        var query = "DELETE FROM " + tableName + " WHERE " + condition;
+
+        connection.query(query, function(err, resultsArr) {
+            if (err) {
+                throw err;
+            }
+            cbFunction(resultsArr);
+        });
+    }
 };
 
 module.exports = orm;
